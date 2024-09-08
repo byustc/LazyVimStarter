a89d153a787fb43d1d1f58f25e75626fdfacd0c5
byustc <byu.ustc@gmail.com>
2024-09-08 09:19:18 +0800

    modify lua config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local unmap = vim.keymap.del
+local nmap = vim.keymap.set
+
+nmap("i", "jj", "<ESC>", { desc = "Esc insert model" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.clipboard = "unnamedplus"
+opt.tabstop = 4
+opt.shiftwidth = 4
+
+opt.spelllang = { "en", "cjk" } -- 设置拼写检查，防止中文提示拼写错误
+opt.spelloptions = "camel"
+opt.scrolloff = 5
+opt.indentexpr = ""
+opt.foldmethod = "indent"
+opt.foldlevel = 99
+opt.foldenable = true
+opt.foldlevelstart = 99
